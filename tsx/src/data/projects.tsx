0292462d9316f4fc1ee8ee0eaf5fcc6c588e4136
piitszk <piitszkdev@outlook.com>
2025-01-03 16:46:54 -0300

    feat: icon colors

--- tsx/src/data/projects.tsx
+++ tsx/src/data/projects.tsx
@@ -1,11 +1,11 @@
-import { Box, CloudLightning, Combine, Home, ShieldAlert } from 'lucide-react';
+import { Box, CloudLightning, Combine, Home, ShieldAlert, Sword } from 'lucide-react';
 import { Project } from '../types/Project';
 
 export const projects: Project[] = [
   {
     title: "Pendings",
     description: "Permite que a equipe administrativa realize devoluções ao player mesmo que ele esteja desconectado do servidor.",
-    icon: <Box className="w-12 h-12 text-indigo-500" />,
+    icon: <Box className="w-12 h-12 text-blue-500" />,
     tech: ["Lua","TypeScript","React","MySQL"],
     isOpenSource: false,
     github: "",
@@ -13,7 +13,7 @@ export const projects: Project[] = [
   },{
     title: "Weather",
     description: "Gerenciamento de clima feito exclusivamente para streamers com intuito de mudar localmente o horário e clima.",
-    icon: <CloudLightning className="w-12 h-12 text-purple-500" />,
+    icon: <CloudLightning className="w-12 h-12 text-yellow-500" />,
     tech: ["Lua","TypeScript","React"],
     isOpenSource: false,
     github: "",
@@ -21,14 +21,14 @@ export const projects: Project[] = [
   },{
     title: "Collectible",
     description: "Colecionáveis que podem ser adquiridos em eventos ou em ocasiões específicas definidas pela equipe administrativa, os colecionáveis podem ser segurados na mão ao utilizar e/ou trocados com outros jogadores.",
-    icon: <Combine className="w-12 h-12 text-purple-500" />,
+    icon: <Combine className="w-12 h-12 text-yellow-600" />,
     tech: ["Lua","TypeScript","React"],
     isOpenSource: false,
     github: "",
   },{
     title: "Residents",
     description: "Permite aos jogadores gerenciem os residentes de suas próprias residências, adicionando e/ou removendo um novo morador ou pagando as taxas, promovendo maior controle.",
-    icon: <Home className="w-12 h-12 text-purple-500" />,
+    icon: <Home className="w-12 h-12 text-green-500" />,
     tech: ["Lua", "TypeScript", "React"],
     isOpenSource: false,
     github: "",
@@ -36,13 +36,13 @@ export const projects: Project[] = [
   },{
     title: "Perimeter",
     description: "Lugares pré-definidos que podem ser acionados pela policia do servidor, informando se o local está com o perímetro fechado ou aberto, notificando a todos.",
-    icon: <ShieldAlert className="w-12 h-12 text-purple-500" />,
+    icon: <ShieldAlert className="w-12 h-12 text-orange-500" />,
     tech: ["Lua"],
     isOpenSource: false,
   },{
     title: "Domination",
     description: "Sistema automático que cria zonas de disputa em horários específicos. Apenas jogadores de uma mesma organização ou sozinhos podem conquistar o território ao permanecer na área, pausando a dominação se houver rivais presentes.",
-    icon: <ShieldAlert className="w-12 h-12 text-purple-500" />,
+    icon: <Sword className="w-12 h-12 text-red-500" />,
     tech: ["Lua","TypeScript","React"],
     isOpenSource: false,
   },
